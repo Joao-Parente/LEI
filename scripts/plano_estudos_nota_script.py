ef896365032a1a8b8532974d7506d886dadb5baf
Gonçalo Pinto <goncalopinto9@gmail.com>
2021-04-29 16:09:25 +0100

    Script ficou com a indentação mal.  no commit passado.
    Corrigido

--- scripts/plano_estudos_nota_script.py
+++ scripts/plano_estudos_nota_script.py
@@ -13,28 +13,30 @@ version = common.version()
 print('=== Version :: ' + str(version) + "      ===")
 
 uid = common.authenticate(db, username, password, {})
-print('=== UID :: ' + str(uid) + "      ===")
+print('=== UID :: ' + str(uid) + "      ===\n")
 
 with open('../dados/notasalunos.json', mode='r', encoding='utf8') as ficheiro:
-    dados = json.load(ficheiro)
-    counter = 0
+	dados = json.load(ficheiro)
+	counter = 0
 
-    for nota in dados:
-        aluno = models.execute_kw(db, uid, password, 'transum.aluno', 'search_read', 
+	for nota in dados:
+		aluno = models.execute_kw(db, uid, password, 'transum.aluno', 'search_read', 
             [[['nr_mecanografico', '=', nota['numero']]]],
             {'fields': ['planos_atuais']})
-
-        plano_estudos_id = models.execute_kw(db, uid, password, 'transum.plano_estudos', 'search_read',
+		
+		plano_estudos_id = models.execute_kw(db, uid, password, 'transum.plano_estudos', 'search_read',
 			[[['id', '=', aluno[0]['planos_atuais'][0]]]],
             {'fields': ['id']})
-
-        uc_pln_stds = models.execute_kw(db, uid, password, 'transum.plano_estudos_uc', 'search_read', [[
+		
+		uc_pln_stds = models.execute_kw(db, uid, password, 'transum.plano_estudos_uc', 'search_read', [[
 			'&', ('plano_estudos.id', '=', plano_estudos_id[0]['id']), ('codigo', '=', nota['uc'])]],
 			{'fields': ['codigo', 'nota']})
-
-        models.execute_kw(db, uid, password, 'transum.plano_estudos_uc', 'write',
+		
+		models.execute_kw(db, uid, password, 'transum.plano_estudos_uc', 'write',
 			[[uc_pln_stds[0]['id']], 
 			{'nota': nota['nota']}])
+		
+		counter += 1
+		print('=== UPDATE Nº ' + str(counter) + '	===')
 
-        numero += 1
-        print('=== UPDATE Nº ' + str(numero) + '	===')
+print('\n=== Th-th-th-that\'s all folks! ===\n')
